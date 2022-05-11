1f1be4d05f6199c8b2f845a92efb89272a41012c
unknown <miroenlared@gmail.com>
2022-05-10 19:37:48 -0500

    avance de estrucutura, antes de slick slider

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,6 +1,8 @@
+import React, { useState } from "react";
 import "../styles/button.css"
 
 function Button (props){
+    const [text, setText] = useState('');
     return (
         <button>{props.text}</button>
     );
--- src/containers/banner.jsx
+++ src/containers/banner.jsx
@@ -5,7 +5,11 @@ import"../styles/banner.css"
 const Banner = () => {
     return(
         <div className="banner">
-            <h2>aqui va estar el texto de banner</h2>
+            <button className="banner--arrow banner--arrow__left"></button>
+            <button className="banner--arrow banner--arrow__right"></button>
+            <h2 className="banner--title">Aqui va estar el texto de banner</h2>
+            <p className="banner--description">Este es el texto de las descripción</p>
+            <button className="banner--btn">View More</button>
         </div>
     )
 }
