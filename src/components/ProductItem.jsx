394d36e6485dcaba7dac5eee6bc82ff75a4da081
unknown <miroenlared@gmail.com>
2022-05-15 19:01:02 -0500

    llamada a fakestore con axios para grilla de productos home

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -8,7 +8,7 @@ const ProductItem = (props) =>{
             <div className="productGrid-item">
                     <img className="productGrid-image" src={props.image} />
                     <h2 className="productGrid-title">{props.title}</h2>
-                    <p className="productGrid-description">{props.description}</p>
+                    {/* <p className="productGrid-description">{props.description}</p> */}
                     <div className="productGrid-price">$ <b>{props.price}</b></div>
             </div>
         </React.Fragment>
--- src/containers/homeProductsGrid.jsx
+++ src/containers/homeProductsGrid.jsx
@@ -1,48 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import ProductItem from "../components/ProductItem";
 import '../styles/productGrid.css'
 
 
 const ProductGrid = () => {
+    const [list, setList] = useState([]);
+    useEffect(()=>{
+        Axios({
+            url:"https://fakestoreapi.com/products/",
+        })
+        .then((response)=>{
+            console.log(response.data)
+            setList(response.data)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    },[setList]);
     return(
         <React.Fragment>
             <div className="productGrid-wrap">
-                <ProductItem 
-                image={"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"}
-                title={"Este es el titulo del producto"}
-                description={"Esta es la descripcion del producto"}
-                price={"0.000"}
-                ></ProductItem>
-
-
-<ProductItem 
-                image={"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"}
-                title={"Este es el titulo del producto"}
-                description={"Esta es la descripcion del producto"}
-                price={"0.000"}
-                ></ProductItem>
-
-<ProductItem 
-                image={"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"}
-                title={"Este es el titulo del producto"}
-                description={"Esta es la descripcion del producto"}
-                price={"0.000"}
-                ></ProductItem>
-
-
-    <ProductItem 
-                image={"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"}
-                title={"Este es el titulo del producto"}
-                description={"Esta es la descripcion del producto"}
-                price={"0.000"}
-                ></ProductItem>
+                {list.map((item)=>(
+                    <ProductItem 
+                    key={item.id}
+                    image={item.image}
+                    title={item.title}
+                    description={item.description}
+                    price={item.price}
+                    ></ProductItem>
+                ))}
             </div>
         </React.Fragment>
-
-
-
-
-        
     )
 }
 
