8a6443a05932f47e5eb75bfa5535212b05541e3f
unknown <miroenlared@gmail.com>
2022-05-16 22:05:40 -0500

    ajustes graficos main wrap

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import "../styles/productItem.css"
 
-
 const ProductItem = (props) =>{
     return(
         <React.Fragment>
             <div className="productGrid-item">
-                    <img className="productGrid-image" src={props.image} />
+                    <img className="productGrid-image" src={props.image} alt={props.title} />
                     <h2 className="productGrid-title">{props.title}</h2>
                     {/* <p className="productGrid-description">{props.description}</p> */}
                     <div className="productGrid-price">$ <b>{props.price}</b></div>
+                    <button className="productGrid-btnAddCart">Add to cart</button>
             </div>
         </React.Fragment>
     )
 }
-
-
 export default ProductItem
